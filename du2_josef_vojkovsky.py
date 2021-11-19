bcd554c07e29cbfa0cbf45338de98857c9648f6b
Josef Vojkovský <pepavoj@email.cz>
2021-11-19 23:43:18 +0100

    ze by?

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,9 +1,13 @@
 import csv
+import math
 with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
     sum = 0
+    pocet_radku = 0
     for row in reader:
+        pocet_radku += 1 
         radek_cislo = float(row[-1].strip())
         sum += radek_cislo
-        writer.writerow()
+    print(math.ceil(pocet_radku/7))
+        # writer.writerow()
