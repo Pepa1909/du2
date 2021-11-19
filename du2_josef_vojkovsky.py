fa252cd7def9ee0b9514eb1d404cbda3f621b993
Josef Vojkovský <pepavoj@email.cz>
2021-11-19 23:11:46 +0100

    neco malo

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,7 +1,9 @@
 import csv
-from os import write
 with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
+    sum = 0
     for row in reader:
-        writer.writerow([row[-1]])
+        radek_cislo = float(row[-1].strip())
+        sum += radek_cislo
+        writer.writerow()
