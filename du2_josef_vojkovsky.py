18280ec3f57b98a37c56e3556275dfd0a39b2f02
Josef Vojkovský <pepavoj@email.cz>
2021-11-20 22:57:44 +0100

    koenčně

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,12 +1,22 @@
 import csv
-import math
+import statistics
 with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
-    sum = 0
-    step = 7
+    l = []
     for row in reader:
-        cislo_na_radku = float(row[-1].strip())
-        sum += cislo_na_radku
-        if reader.line_num == 1 or reader.line_num % 7 == 1:
-            writer.writerow(row[0:-1] + [row[-1].strip()])
+        l.append(float(row[-1].strip()))
+prumer1 = statistics.mean(l[0:7])
+prumer2 = statistics.mean(l[7:14])
+prumer3 = statistics.mean(l[14:])
+with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    writer = csv.writer(csvoutfile)
+    for row in reader:
+        if reader.line_num == 1:
+            writer.writerow(row[0:-1] + [format(prumer1, ".4f")])
+        elif reader.line_num == 8:
+            writer.writerow(row[0:-1] + [format(prumer2, ".4f")])
+        elif reader.line_num == 15:
+            writer.writerow(row[0:-1] + [format(prumer3,".4f")])
+    print(f"{prumer1:.4f}, {prumer2:.4f}, {prumer3:.4f}")
