ea2055370ddd7104c3d9f434a4a1789be1f0cb39
Josef Vojkovský <pepavoj@email.cz>
2021-11-22 18:20:36 +0100

    konečně

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,22 +1,19 @@
 import csv
 import statistics
+import math
 with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
-    l = []
+    l_tyden = []
     for row in reader:
-        l.append(float(row[-1].strip()))
-prumer1 = statistics.mean(l[0:7])
-prumer2 = statistics.mean(l[7:14])
-prumer3 = statistics.mean(l[14:])
-with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
-    reader = csv.reader(csvfile, delimiter = ",")
-    writer = csv.writer(csvoutfile)
-    for row in reader:
-        if reader.line_num == 1:
-            writer.writerow(row[0:-1] + [format(prumer1, ".4f")])
-        elif reader.line_num == 8:
-            writer.writerow(row[0:-1] + [format(prumer2, ".4f")])
-        elif reader.line_num == 15:
-            writer.writerow(row[0:-1] + [format(prumer3,".4f")])
-    print(f"{prumer1:.4f}, {prumer2:.4f}, {prumer3:.4f}")
+        prvni_radka = row
+        l_tyden.append(float(row[-1].strip()))
+        for _ in range(math.ceil(len(l_tyden)/7)):
+            if len(l_tyden) == 7:
+                prumer = statistics.mean(l_tyden)
+                writer.writerow(row[0:-1] + [format(prumer, ".4f")])
+                l_tyden=[]
+    if len(l_tyden) > 0:
+        prumer = statistics.mean(l_tyden)
+        writer.writerow(prvni_radka[0:-1] + [format(prumer, ".4f")])
+    
