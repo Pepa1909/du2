6d25c059253fd3154a63436581d1d96558ed02d2
Josef Vojkovský <pepavoj@email.cz>
2021-11-23 23:28:17 +0100

    jop

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,19 +1,20 @@
 import csv
 import statistics
 import math
-with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
+with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
     l_tyden = []
+    radky = []
     for row in reader:
-        prvni_radka = row
+        radky.append(row[0:-1])
         l_tyden.append(float(row[-1].strip()))
         for _ in range(math.ceil(len(l_tyden)/7)):
             if len(l_tyden) == 7:
                 prumer = statistics.mean(l_tyden)
-                writer.writerow(row[0:-1] + [format(prumer, ".4f")])
+                writer.writerow(radky[0] + [format(prumer, ".4f")])
                 l_tyden=[]
+                radky = []
     if len(l_tyden) > 0:
         prumer = statistics.mean(l_tyden)
-        writer.writerow(prvni_radka[0:-1] + [format(prumer, ".4f")])
-    
+        writer.writerow(radky[0] + [format(prumer, ".4f")])
