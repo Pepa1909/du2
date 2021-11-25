1d4b2317bfa1a2425d06f3d2a89b6bd3739a7862
Josef Vojkovský <pepavoj@email.cz>
2021-11-26 00:16:54 +0100

    finální bez bonusu

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -1,12 +1,17 @@
+# naimportování potřebných modulů
 import csv
 import statistics
 import math
 
+# definování několika funkcí, které se opakují pro oba výstupy
+
+# funkce na započítání a správné zapsání posledního řádku
 def zbytek(hodnota, prumer, radek_list, writer):
     if len(hodnota) > 0:
         prumer = statistics.mean(hodnota)
         writer.writerow(radek_list[0] + ["   "+ format(prumer, ".4f")])
 
+# funkce, která ošetřuje chybějící hodnotu průtoku (= poslední sloupec)
 def try_block(hodnota, row):
     try:
         hodnota.append(float(row[-1]))
@@ -14,41 +19,53 @@ def try_block(hodnota, row):
         print(f"chybí data za den {row[2:-1]}. Prosím doplňte.")
         quit()
 
+#funkce, která ošetřuje duplicitní dny ve vstupu
 def duplikat(row):
-    print(f"datový soubor obsahuje duplikát, den {row[2:-1]} je zapsaný dvakrát. Program se ukončí")
-    quit()
+    if len(radek_tyden_list) > 1 and radek_tyden_list[-2] == radek_tyden_list[-1]:
+        print(f"datový soubor obsahuje duplikát, den {row[2:-1]} je zapsaný dvakrát. Program se ukončí")
+        quit()
 
+# otevření souborů na sedmidenní průměr
 with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_7dni.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
+    # vytvoření listů na ukládání klíčových hodnot
     hodnota_prutoku_tyden = []
     radek_tyden_list = []
+    # for cyklus, který projede celý .csv soubor
     for row_tyden in reader:
         radek_tyden_list.append(row_tyden[0:-1])
         try_block(hodnota_prutoku_tyden,row_tyden)
-        for _ in range(math.ceil(len(hodnota_prutoku_tyden)/7)):
-            if len(radek_tyden_list) > 1 and radek_tyden_list[-2] == radek_tyden_list[-1]:
-                duplikat(row_tyden)
+        # vložený for cyklus, který se stane (počet řádků ve vstupním souboru)/7-krát, ale dělení je celočíselné
+        for _ in range(len(hodnota_prutoku_tyden)//7):
+            duplikat(row_tyden)
+            # pokud je délka seznamu hodnot 7, udělá se z nich průměr, řádek se vytiskne do výstupu, seznamy se vyčistí a jede se znovu
             if len(hodnota_prutoku_tyden) == 7:
                 prumer_tyden = statistics.mean(hodnota_prutoku_tyden)
                 writer.writerow(radek_tyden_list[0] + ["   "+ format(prumer_tyden, ".4f")])
                 hodnota_prutoku_tyden=[]
                 radek_tyden_list = []
+    # toto se stane, když v souboru nezbývá 7 dnů
     zbytek(hodnota_prutoku_tyden, prumer_tyden, radek_tyden_list, writer)
 
+# otevření souborů na roční průměr
 with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_rok.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
+    # vytvoření seznamů, oproti týdnu je tu i seznam, který uchovává o jaký rok se právě jedná
     hodnota_prutoku_rok = []
     radek_rok_list = []
     rok = []
+    # for cyklus, který projede celý soubor
     for row_rok in reader:
         radek_rok_list.append(row_rok[0:-1])
         try_block(hodnota_prutoku_rok,row_rok)
         rok.append(int(row_rok[2]))
+        # podobně jako u týdenního průměru, toto se stane (počet řádků ve vstupním souboru)/365-krát, zaokrouhleno nahoru
         for _ in  range(math.ceil(len(hodnota_prutoku_rok)/365)):
-            if len(radek_rok_list) > 1 and radek_rok_list[-2] == radek_rok_list[-1]:
-                duplikat(row_tyden)
+            duplikat(row_tyden)
+            # když jsou v seznamu roků alespoň 2 hodnoty a poslední se nerovná předposlední, nastane odebrání posledního řádku (už jiný rok)
+            # a vypočítá se průměr z předešlých hodnot; pak se seznamy vyčistí, ale ponechá se v nich odebraný řádek z předešlého roku (jedná se totiž o 1. leden nového roku)
             if len(rok) > 1 and rok[-2] != rok[-1]:
                 prvni_hodnota=hodnota_prutoku_rok.pop()
                 prvni_radek=radek_rok_list.pop()
@@ -57,4 +74,5 @@ with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_rok.csv", "w",
                 hodnota_prutoku_rok = [prvni_hodnota]
                 rok = []
                 radek_rok_list = [prvni_radek]
+    # provede se pro poslední rok v záznamu, jinak by chyběl (druhá část podmínky z řádku 69 už nemůže nastat)
     zbytek(hodnota_prutoku_rok, prumer_rok, radek_rok_list, writer)
