a9ba41c52c684d114ce841fb10004b03aa02c6be
Josef Vojkovský <pepavoj@email.cz>
2021-11-27 15:05:32 +0100

    Issues opraveny

--- du2_josef_vojkovsky.py
+++ du2_josef_vojkovsky.py
@@ -20,8 +20,8 @@ def try_block(hodnota, row):
         quit()
 
 # funkce, která ošetřuje duplicitní dny ve vstupu
-def duplikat(row):
-    if len(radek_tyden_list) > 1 and radek_tyden_list[-2] == radek_tyden_list[-1]:
+def duplikat(row, radek_list):
+    if len(radek_list) > 1 and radek_list[-2] == radek_list[-1]:
         print(f"datový soubor obsahuje duplikát, den {row[2:-1]} je zapsaný dvakrát. Program se ukončí.")
         quit()
 
@@ -36,59 +36,73 @@ def extremy(prutok_list):
             min_prutok = hodnoty_prutok
     return max_prutok, min_prutok
 
-# otevření souborů na sedmidenní průměr
-with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_7dni.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
-    reader = csv.reader(csvfile, delimiter = ",")
-    writer = csv.writer(csvoutfile)
-    # vytvoření listů na ukládání klíčových hodnot
-    hodnota_prutoku_tyden = []
-    radek_tyden_list = []
-    prutok_list = []
-    # for cyklus, který projede celý .csv soubor
-    for row_tyden in reader:
-        prutok_list.append((row_tyden))
-        radek_tyden_list.append(row_tyden[0:-1])
-        try_block(hodnota_prutoku_tyden,row_tyden)
-        # vložený for cyklus, který se stane (počet řádků ve vstupním souboru)/7-krát, ale dělení je celočíselné
-        for _ in range(len(hodnota_prutoku_tyden)//7):
-            duplikat(row_tyden)
-            # pokud je délka seznamu hodnot 7, udělá se z nich průměr, řádek se vytiskne do výstupu, seznamy se vyčistí a jede se znovu
-            if len(hodnota_prutoku_tyden) == 7:
-                prumer_tyden = statistics.mean(hodnota_prutoku_tyden)
-                writer.writerow(radek_tyden_list[0] + ["   "+ format(prumer_tyden, ".4f")])
-                hodnota_prutoku_tyden=[]
-                radek_tyden_list = []
-    # toto se stane, když v souboru nezbývá 7 dnů
-    zbytek(hodnota_prutoku_tyden, prumer_tyden, radek_tyden_list, writer)
-    # část, která se postará o extrémy v daném souboru
-    max_prutok, min_prutok = extremy(prutok_list)
-    print(f"nejvyšší denní průtok byl {max_prutok[-1].strip()} dne {max_prutok[2:-1]}, nejnižší denní průtok byl {min_prutok[-1].strip()} dne {min_prutok[2:-1]}")
+# sedmidenní průměr
+try:
+    with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_7dni.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
+        reader = csv.reader(csvfile, delimiter = ",")
+        writer = csv.writer(csvoutfile)
+        # vytvoření listů na ukládání klíčových hodnot
+        hodnota_prutoku_tyden = []
+        radek_tyden_list = []
+        prutok_list = []
+        # for cyklus, který projede celý .csv soubor
+        for row_tyden in reader:
+            prutok_list.append((row_tyden))
+            radek_tyden_list.append(row_tyden[0:-1])
+            try_block(hodnota_prutoku_tyden,row_tyden)
+            # vložený for cyklus, který se stane (počet řádků ve vstupním souboru)/7-krát, ale dělení je celočíselné
+            for _ in range(len(hodnota_prutoku_tyden)//7):
+                duplikat(row_tyden,radek_tyden_list)
+                # pokud je délka seznamu hodnot 7, udělá se z nich průměr, řádek se vytiskne do výstupu, seznamy se vyčistí a jede se znovu
+                if len(hodnota_prutoku_tyden) == 7:
+                    prumer_tyden = statistics.mean(hodnota_prutoku_tyden)
+                    writer.writerow(radek_tyden_list[0] + ["   "+ format(prumer_tyden, ".4f")])
+                    hodnota_prutoku_tyden=[]
+                    radek_tyden_list = []
+        # toto se stane, když v souboru nezbývá 7 dnů
+        zbytek(hodnota_prutoku_tyden, prumer_tyden, radek_tyden_list, writer)
+        # část, která se postará o extrémy v daném souboru
+        max_prutok, min_prutok = extremy(prutok_list)
+        print(f"nejvyšší denní průtok byl {max_prutok[-1].strip()} dne {max_prutok[2:-1]}, nejnižší denní průtok byl {min_prutok[-1].strip()} dne {min_prutok[2:-1]}")
+except FileNotFoundError:
+    print("Soubor nenalezen. Soubor .csv se musí nacházet ve stejné složce jako tento program.")
+    quit()
+except PermissionError:
+    print("Program nema oprávnění ke čtení souboru, upravte pravidla nebo zkuste vše přesunout do jiné složky.")
+    quit()
 
-# otevření souborů na roční průměr
-with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_rok.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
-    reader = csv.reader(csvfile, delimiter = ",")
-    writer = csv.writer(csvoutfile)
-    # vytvoření seznamů, oproti týdnu je tu i seznam, který uchovává o jaký rok se právě jedná
-    hodnota_prutoku_rok = []
-    radek_rok_list = []
-    rok = []
-    # for cyklus, který projede celý soubor
-    for row_rok in reader:
-        radek_rok_list.append(row_rok[0:-1])
-        try_block(hodnota_prutoku_rok,row_rok)
-        rok.append(int(row_rok[2]))
-        # podobně jako u týdenního průměru, toto se stane (počet řádků ve vstupním souboru)/365-krát, zaokrouhleno nahoru
-        for _ in  range(math.ceil(len(hodnota_prutoku_rok)/365)):
-            duplikat(row_tyden)
-            # když jsou v seznamu roků alespoň 2 hodnoty a poslední se nerovná předposlední, nastane odebrání posledního řádku (už jiný rok) a vypočítá se
-            # průměr z předešlých hodnot; pak se seznamy vyčistí, ale ponechá se v nich odebraný řádek z předešlého roku (jedná se totiž o 1. leden nového roku)
-            if len(rok) > 1 and rok[-2] != rok[-1]:
-                prvni_hodnota=hodnota_prutoku_rok.pop()
-                prvni_radek=radek_rok_list.pop()
-                prumer_rok = statistics.mean(hodnota_prutoku_rok)
-                writer.writerow(radek_rok_list[0]+["   "+ format(prumer_rok, ".4f")])
-                hodnota_prutoku_rok = [prvni_hodnota]
-                rok = []
-                radek_rok_list = [prvni_radek]
-    # provede se pro poslední rok v záznamu, jinak by chyběl (druhá část podmínky z řádku 85 už nemůže nastat)
-    zbytek(hodnota_prutoku_rok, prumer_rok, radek_rok_list, writer)
+# roční průměr
+try:
+    with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_rok.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
+        reader = csv.reader(csvfile, delimiter = ",")
+        writer = csv.writer(csvoutfile)
+        # vytvoření seznamů, oproti týdnu je tu i seznam, který uchovává o jaký rok se právě jedná
+        hodnota_prutoku_rok = []
+        radek_rok_list = []
+        rok = []
+        # for cyklus, který projede celý soubor
+        for row_rok in reader:
+            radek_rok_list.append(row_rok[0:-1])
+            try_block(hodnota_prutoku_rok,row_rok)
+            rok.append(int(row_rok[2]))
+            # podobně jako u týdenního průměru, toto se stane (počet řádků ve vstupním souboru)/365-krát, zaokrouhleno nahoru
+            for _ in  range(math.ceil(len(hodnota_prutoku_rok)/365)):
+                duplikat(row_tyden, radek_rok_list)
+                # když jsou v seznamu roků alespoň 2 hodnoty a poslední se nerovná předposlední, nastane odebrání posledního řádku (už jiný rok) a vypočítá se
+                # průměr z předešlých hodnot; pak se seznamy vyčistí, ale ponechá se v nich odebraný řádek z předešlého roku (jedná se totiž o 1. leden nového roku)
+                if len(rok) > 1 and rok[-2] != rok[-1]:
+                    prvni_hodnota=hodnota_prutoku_rok.pop()
+                    prvni_radek=radek_rok_list.pop()
+                    prumer_rok = statistics.mean(hodnota_prutoku_rok)
+                    writer.writerow(radek_rok_list[0]+["   "+ format(prumer_rok, ".4f")])
+                    hodnota_prutoku_rok = [prvni_hodnota]
+                    rok = []
+                    radek_rok_list = [prvni_radek]
+        # provede se pro poslední rok v záznamu, jinak by chyběl (druhá část podmínky z řádku 85 už nemůže nastat)
+        zbytek(hodnota_prutoku_rok, prumer_rok, radek_rok_list, writer)
+except FileNotFoundError:
+    print("Soubor nenalezen. Soubor .csv se musí nacházet ve stejné složce jako tento program.")
+    quit()
+except PermissionError:
+    print("Program nema oprávnění ke čtení souboru, upravte pravidla nebo zkuste vše přesunout do jiné složky.")
+    quit()
